8389be7bc94e29e410cd6a0fdacf1e2be03630d6
PhakawatL <ipeemy@gmail.com>
2023-04-22 16:02:44 +0700

    Delete Exercise4_Phakawat_L.py

--- Exercise4_Phakawat_L.py
+++
@@ -1,6 +0,0 @@
-โจทย์ = "โจทย์ : ให้ผู้เรียนพัฒนาโปรแกรมโดยสร้างตัวแปรสำหรับเก็บคะแนนผู้เรียน"
-print(โจทย์)
-
-Sccore_FoundationEnglish = 65.5
-Sccore_GeneralBusiness = 86
-Sccore_IntroductiontoComputerSystems = 71.5
