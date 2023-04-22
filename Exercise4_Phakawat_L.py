ba0dd742c44742143ba704e6ca789111c46a767b
PhakawatL <ipeemy@gmail.com>
2023-04-22 15:52:22 +0700

    โจทย์
    +โจทย์

---
+++ Exercise4_Phakawat_L.py
@@ -0,0 +1,12 @@
+print(โจทย์)
+
+Sccore_FoundationEnglish = 65.5
+Sccore_GeneralBusiness = 86
+Sccore_IntroductiontoComputerSystems = 71.5
+Sccore_ComputerProgramming = 81
+
+print("--- Your Score ---")
+print("Foundation English               :",Sccore_FoundationEnglish)
+print("General Business                 :",Sccore_GeneralBusiness)
+print("Introduction to Computer Systems :",Sccore_IntroductiontoComputerSystems)
+print("Computer Programming             :",Sccore_ComputerProgramming)
